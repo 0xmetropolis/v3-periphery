bd737ed0c8b170203837bdf2ddfd76ccb6c79e6c
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2024-06-26 11:25:03 -0600

    remove whitespace

--- contracts/NonfungibleTokenPositionDescriptor.sol
+++ contracts/NonfungibleTokenPositionDescriptor.sol
@@ -57,7 +57,7 @@ contract NonfungibleTokenPositionDescriptor is INonfungibleTokenPositionDescript
         (, , address token0, address token1, uint24 fee, int24 tickLower, int24 tickUpper, , , , , ) =
             positionManager.positions(tokenId);
 
-        IUniswapV3Pool pool = 
+        IUniswapV3Pool pool =
             IUniswapV3Pool(
                 PoolAddress.computeAddress(
                     positionManager.factory(),
